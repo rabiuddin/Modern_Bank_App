b5eb6344e5cfb3bca58c2d9565d10cec90f0ea5a
Rabi Uddin <rabiuddin1@gmail.com>
2024-07-16 18:47:19 +0500

    Completed the Navbar and Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./style";
 
-import { Navbar, Hero, Stats, Bussiness, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
@@ -16,7 +16,7 @@ const App = () => (
     <div className={`bg-primary ${styles.flexStart} $${styles.boxWidth}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
-        <Bussiness />
+        <Business />
         <Billing />
         <CardDeal />
         <Testimonials />
