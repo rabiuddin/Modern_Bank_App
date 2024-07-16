c4d5e54e3a970a3f556f25d267d44e70a01a235e
Rabi Uddin <rabiuddin1@gmail.com>
2024-07-16 16:34:57 +0500

    Settedup Folder structure for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import styles from "./style";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Navbar, Hero, Stats, Bussiness, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}><Navbar /></div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}><Hero /></div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart} $${styles.boxWidth}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Bussiness />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  </div>
+);
 
-export default App
+export default App;
