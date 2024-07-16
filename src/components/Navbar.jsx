4b23ec36b9ce389cd5d3118974bd8ce7120750b1
Rabi Uddin <rabiuddin1@gmail.com>
2024-07-16 17:08:50 +0500

    Changed name of folder "assets"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <nav className="w-full flex py-6 justify-between items-center"></nav>;
+};
 
-export default Navbar
+export default Navbar;
