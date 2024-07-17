f34fc6f16f5120833d621ae22d8ae0a2a4d24f16
Rabi Uddin <rabiuddin1@gmail.com>
2024-07-17 13:12:37 +0500

    Completed the  Stats and Business section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import styles from "./style";
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
+  <div className="bg-primary w-full overflow-hidden px-10">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}><Navbar /></div>
     </div>
